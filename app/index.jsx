db32234f84aaf80fe21d7f41b5fe234cee447133
jaymin <jaymin8973@gmail.com>
2025-09-23 19:15:21 +0530

    Code

--- app/index.jsx
+++ app/index.jsx
@@ -17,8 +17,8 @@ export default function Index() {
           />
         </View>
         <View className=" h-1/2  px-5 gap-5 ">
-          <Text className=" text-5xl font-bold text-center mt-10">Welcome 👋</Text>
-          <Text className=" text-lg text-center mt-2 ">Don't miss a goal! Get live match notifications and match results from your favourite teams and world's most prestigious competitions!</Text>
+          <Text className=" text-4xl font-bold text-center mt-10">Welcome 👋</Text>
+          <Text className=" text-center mt-2 ">Don't miss a goal! Get live match notifications and match results from your favourite teams and world's most prestigious competitions!</Text>
           <Pressable className="bg-black rounded-full py-5 mt-10" onPress={() => router.push("(Authentication)/Signup")}>
             <Text className="text-white text-lg font-bold text-center">Get Started</Text>
           </Pressable>
